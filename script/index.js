4c7a95194a499570692a2b69b24153ea36a1a912
mjontop <mjontop0602@gmail.com>
2019-09-07 20:57:03 +0530

    image centered using jquery

--- script/index.js
+++ script/index.js
@@ -1,4 +1,7 @@
 let resizer = () => {
+	pixel = ($("#myimg").width() - $("#img").width()) / 2;
+
+	$("#img").css("margin-left", pixel);
 	if ($(window).width() < 575) {
 		$("#bottom").removeClass("container");
 	} else {
