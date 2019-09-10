e246658988bd4da439ee9752de698fea08e1244c
mjontop <mjontop0602@gmail.com>
2019-09-10 15:52:43 +0530

    contact tab created

--- script/index.js
+++ script/index.js
@@ -1,12 +1,42 @@
 let resizer = () => {
-	pixel = ($("#myimg").width() - $("#img").width()) / 2;
+	pixel = ($("#myimg").width() - $("#img").width()) / 2
 
-	$("#img").css("margin-left", pixel);
+	$("#img").css("margin-left", pixel)
 	if ($(window).width() < 575) {
-		$("#bottom").removeClass("container");
+		$("#bottom").removeClass("container")
 	} else {
-		$("#bottom").addClass("container");
+		$("#bottom").addClass("container")
 	}
-};
+}
 
-$(window).on("resize", resizer);
+$(window).on("resize", resizer)
+
+$(document).ready(function() {
+	var open = "home"
+
+	$("#navhome").click(function() {
+		if (open !== "home") {
+			$(".hiding").slideUp()
+			$(".intro").slideDown()
+			open = "home"
+		}
+	})
+	$("#navuser").click(function() {
+		if (open !== "user") {
+			$(".user").removeClass("d-none")
+			$(".contact").removeClass("d-none")
+			$(".hiding").slideUp()
+			$(".user").slideDown()
+			open = "user"
+		}
+	})
+	$("#navcontact").click(function() {
+		if (open !== "contact") {
+			$(".user").removeClass("d-none")
+			$(".contact").removeClass("d-none")
+			$(".hiding").slideUp()
+			$(".contact").slideDown()
+			open = "contact"
+		}
+	})
+})
